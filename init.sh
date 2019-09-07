5305025cc81042010c736d9d80b7d3f7768a5f4f
Aleksgav <alex.phyp@gmail.com>
2019-09-07 06:01:58 +0300

    added systemwide .gitignore .gitigmore-system

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 
 export DOTFILES=$HOME/.dotfiles
 
+ln -s $DOTFILES/.gitignore-system $HOME/.gitignore-system
+ln -s $DOTFILES/.gitconfig   $HOME/.gitconfig
 ln -s $DOTFILES/.tmux.conf   $HOME/.tmux.conf
 ln -s $DOTFILES/.vimrc       $HOME/.vimrc
 
