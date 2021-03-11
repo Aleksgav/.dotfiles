7ed62341373090885dc5e4000963194c5a1929f7
Aleks G <alex.phyp@gmail.com>
2021-03-11 21:47:21 +0300

    [feat]: added Brewfile

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 
 export DOTFILES=$HOME/.dotfiles
 
+ln -s $DOTFILES/Brewfile $HOME/Brewfile
 ln -s $DOTFILES/.gitignore-system $HOME/.gitignore-system
 ln -s $DOTFILES/.gitconfig   $HOME/.gitconfig
 ln -s $DOTFILES/.tmux.conf   $HOME/.tmux.conf
