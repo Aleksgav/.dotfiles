801454c28874a536e299ba0213e7bc45466fafb7
Gavrishchuk Aleksandr <alex.phyp@gmail.com>
2021-11-05 01:26:32 +0300

    [feat]: added delta

--- init.sh
+++ init.sh
@@ -12,6 +12,7 @@ ln -s $DOTFILES/.vimrc       $HOME/.vimrc
 ln -s $DOTFILES/.zshrc       $HOME/.zshrc
 ln -s $DOTFILES/.doom.d      $HOME/.doom.d
 ln -s $DOTFILES/.starship.toml $HOME/.starship.toml
+ln -s $DOTFILES/.gitconfig_delta $HOME/.gitconfig_delta
 
 # set up macos settings
 defaults write com.apple.dock springboard-columns -int 10
