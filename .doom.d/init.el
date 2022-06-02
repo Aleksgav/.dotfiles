04bf58a7631d02ff5a9860896d79285213fe10c8
Gavrishchuk Aleksandr <alex.phyp@gmail.com>
2022-06-02 04:21:27 +0300

    [feat]: added forgotten rust lsp

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -167,7 +167,7 @@
        (ruby +lsp
              +rvm
              +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust +lsp)         ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        scheme            ; a fully conniving family of lisps
        (sh +lsp)          ; she sells {ba,z,fi}sh shells on the C xor
