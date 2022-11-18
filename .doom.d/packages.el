a370e2adb9c7ce4340e18e2a0d247619facde3f3
Gavrishchuk Aleksandr <alex.phyp@gmail.com>
2022-11-18 06:48:59 +0300

    [feat]: added protobuf mode

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -16,6 +16,7 @@
 (package! org-bullets)
 (package! treemacs-all-the-icons)
 (package! treemacs-icons-dired)
+(package! protobuf-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
