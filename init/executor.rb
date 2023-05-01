0f3011fea512debdfdc55c6d55e25d09d9e38eeb
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2023-05-01 21:01:21 +0300

    [feat]: added dependent spinner

--- init/executor.rb
+++ init/executor.rb
@@ -5,6 +5,8 @@ require 'tty-box'
 require 'tty-screen'
 require 'tty-spinner'
 
+require_relative 'command_register'
+
 class Executor
   attr_accessor :commands,
                 :success_mark,
@@ -12,30 +14,70 @@ class Executor
                 :err_suffix,
                 :ok_suffix,
                 :spinner_title,
-                :spinner,
+                :spinner_format,
+                :spinner_group_style,
                 :err_printer
 
   def initialize(err_printer)
     @err_printer = err_printer
 
     init_styles
-    init_spinner
     init_state
   end
 
-  def register(description, command)
-    commands.push([description, command])
+  def register(&block)
+    return unless block_given?
+
+    commands_register = CommandRegister.new
+    block[commands_register]
+
+    @commands = commands_register.commands
   end
 
   def run(**options)
     commands.each do |command|
-      exit_status, err_message = exec(*command, **options).value
-
-      err_printer.print(err_message) unless exit_status.zero?
+      case command
+          in { description:, command: } then exec(description, command, **options)
+          in { description:, group: } then exec_group(description, group, **options)
+      end
     end
   end
 
   private
+
+  def exec(title, command, **options)
+    spinner = TTY::Spinner.new(spinner_title, **spinner_format)
+    spinner.update(title: title)
+
+    exec_with_spinner(spinner, command, **options)
+  end
+
+  def exec_group(description, group, **options)
+    sp_group = TTY::Spinner::Multi.new(make_group_title(description), style: spinner_group_style, **spinner_format)
+
+    group.each do |row|
+      row => { description:, command: }
+
+      spinner = sp_group.register(spinner_title, **spinner_format)
+      spinner.update(title: description)
+
+      exec_with_spinner(spinner, command, **options)
+    end
+  end
+
+  def exec_with_spinner(spinner, command, dry_run: false)
+    spinner.run do |spnr|
+      spnr.success(ok_suffix) and next([0, 'success']) if dry_run
+
+      *, stderr, wait_thr = Open3.popen3(command)
+
+      wait_thr.value.exitstatus.zero? ? spnr.success(ok_suffix) : spnr.error(err_suffix)
+
+      [wait_thr.value.exitstatus, stderr.read]
+    rescue Errno::ENOENT => e
+      [1, e.message]
+    end.join
+  end
 
   def init_state
     @commands = []
@@ -53,25 +95,12 @@ class Executor
                      Styles::WHITE['] '] +
                      Styles::WHITE['Installation: '] +
                      Styles::PACKAGE[':title']
+
+    @spinner_format = { format: :dots, success_mark: success_mark, error_mark: error_mark }
+    @spinner_group_style = { top: '', middle: ' ├──', bottom: ' └── ' }
   end
 
-  def init_spinner
-    @spinner = TTY::Spinner.new(spinner_title, format: :dots, success_mark: success_mark, error_mark: error_mark)
-  end
-
-  def exec(title, command, dry_run: false)
-    spinner.update(title: title)
-
-    spinner.run do |spnr|
-      spnr.success(ok_suffix) and next([0, 'success']) if dry_run
-
-      *, stderr, wait_thr = Open3.popen3(command)
-
-      wait_thr.value.exitstatus.zero? ? spnr.success(ok_suffix) : spnr.error(err_suffix)
-
-      [wait_thr.value.exitstatus, stderr.read]
-    rescue Errno::ENOENT => e
-      [1, e.message]
-    end.join
+  def make_group_title(title)
+    Styles::WHITE['['] + Styles::SPINNER[':spinner'] + Styles::WHITE['] '] + Styles::WHITE[title]
   end
 end
--- init/init.rb
+++ init/init.rb
@@ -8,47 +8,55 @@ require_relative 'command_install'
 CONFIG_DIR = '~/.config'
 
 install = Init::CLI::Commands::Install.new do |executor|
-  executor.register('xcode-select', 'xcode-select --install')
-  executor.register('create .config folder', 'mkdir -p ~/.config')
-  executor.register('create projects folder', 'mkdir -p ~/projects')
+  executor.register do |register|
+    register.command('xcode-select', 'xcode-select --install')
 
-  command =<<-CMD
-          export DOTFILES=$HOME/.dotfiles
-          export CONFIG_DIR=#{CONFIG_DIR}
+    register.command_group('create folders') do |group|
+      group.command('create .config folder', 'mkdir -p ~/.config')
+      group.command('create projects folder', 'mkdir -p ~/projects')
+    end
 
-          ln -s $DOTFILES/Brewfile          $HOME/Brewfile
-          ln -s $DOTFILES/.gitignore-system $HOME/.gitignore-system
-          ln -s $DOTFILES/.gitconfig        $HOME/.gitconfig
-          ln -s $DOTFILES/.tmux.conf        $HOME/.tmux.conf
-          ln -s $DOTFILES/.tmux.osx.conf    $HOME/.tmux.osx.conf
-          ln -s $DOTFILES/.tmux.linux.conf  $HOME/.tmux.linux.conf
-          ln -s $DOTFILES/.vimrc            $HOME/.vimrc
-          ln -s $DOTFILES/.zshrc            $HOME/.zshrc
-          ln -s $DOTFILES/.doom.d           $CONFIG_DIR/doom
-          ln -s $DOTFILES/.starship.toml    $CONFIG_DIR/starship.toml
-          ln -s $DOTFILES/.gitconfig_delta  $HOME/.gitconfig_delta
-          ln -s $DOTFILES/sheldon           $CONFIG_DIR/sheldon
-  CMD
-  executor.register('link dotfiles', command)
-  executor.register('Installing brew bundle...', 'brew bundle install -v')
+    command =<<-CMD
+            export DOTFILES=$HOME/.dotfiles
+            export CONFIG_DIR=#{CONFIG_DIR}
 
-  command =<<-CMD
-          defaults write com.apple.dock springboard-columns -int 10
-          defaults write com.apple.dock springboard-rows -int 7
+            ln -s $DOTFILES/Brewfile          $HOME/Brewfile
+            ln -s $DOTFILES/.gitignore-system $HOME/.gitignore-system
+            ln -s $DOTFILES/.gitconfig        $HOME/.gitconfig
+            ln -s $DOTFILES/.tmux.conf        $HOME/.tmux.conf
+            ln -s $DOTFILES/.tmux.osx.conf    $HOME/.tmux.osx.conf
+            ln -s $DOTFILES/.tmux.linux.conf  $HOME/.tmux.linux.conf
+            ln -s $DOTFILES/.vimrc            $HOME/.vimrc
+            ln -s $DOTFILES/.zshrc            $HOME/.zshrc
+            ln -s $DOTFILES/.doom.d           $CONFIG_DIR/doom
+            ln -s $DOTFILES/.starship.toml    $CONFIG_DIR/starship.toml
+            ln -s $DOTFILES/.gitconfig_delta  $HOME/.gitconfig_delta
+            ln -s $DOTFILES/sheldon           $CONFIG_DIR/sheldon
+    CMD
+    register.command('link dotfiles', command)
 
-          defaults write com.apple.Dock autohide 1
+    register.command('Installing brew bundle...', 'brew bundle install -v')
 
-          killall Dock
-  CMD
-  executor.register('mac settings', command)
-  executor.register('init sheldon', 'sheldon lock')
+    command =<<-CMD
+            defaults write com.apple.dock springboard-columns -int 10
+            defaults write com.apple.dock springboard-rows -int 7
 
-  command =<<-CMD
-          git clone --depth 1 https://github.com/doomemacs/doomemacs #{CONFIG_DIR}/emacs
-          #{CONFIG_DIR}/emacs/bin/doom install
-  CMD
-  executor.register('install doom emacs', command)
-  executor.register('emacs icon', 'ln -s /usr/local/opt/emacs-mac/Emacs.app /Applications')
+            defaults write com.apple.Dock autohide 1
+
+            killall Dock
+    CMD
+    register.command('mac settings', command)
+    register.command('init sheldon', 'sheldon lock')
+
+    register.command_group('doom emacs') do |group|
+      command =<<-CMD
+              git clone --depth 1 https://github.com/doomemacs/doomemacs #{CONFIG_DIR}/emacs
+              #{CONFIG_DIR}/emacs/bin/doom install
+      CMD
+      group.command('install doom emacs', command)
+      group.command('emacs icon', 'ln -s /usr/local/opt/emacs-mac/Emacs.app /Applications')
+    end
+  end
 end
 
 Init::CLI::Commands.register 'install', install, aliases: ['i']
