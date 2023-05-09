b72ba4782b8e9ce6424af1eefd34137ad360db1c
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2023-05-09 16:41:00 +0300

    [feat]: ssh-agent -> doom config

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -17,6 +17,7 @@
 (package! treemacs-all-the-icons)
 (package! treemacs-icons-dired)
 (package! protobuf-mode)
+(package! keychain-environment.)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
