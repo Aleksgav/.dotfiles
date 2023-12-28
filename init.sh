b40046a7f0cfec9b479ba6ab7baef94cb460e927
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2023-12-28 04:33:23 +0300

    [feat]: refactor pre install step: added git step

--- init.sh
+++ init.sh
@@ -24,6 +24,29 @@ function linux_distibution_detect {
     esac
 }
 
+function linux_manjaro_install {
+    pamac install git
+
+    echo "Clonning .dotfiles..."
+    git clone https://github.com/Aleksgav/.dotfiles.git
+
+    cd ~/.dotfiles
+
+    . init_linux_manjaro.sh
+}
+
+function osx_install {
+    echo "Install Xcode"
+    xcode-select --install
+
+    echo "Clonning .dotfiles..."
+    git clone https://github.com/Aleksgav/.dotfiles.git
+
+    cd ~/.dotfiles
+
+    . init_osx.sh
+}
+
 echo "Detecting OS..."
 os_detect
 
@@ -36,7 +59,7 @@ if [ "$os" = "UNKNOWN" ]; then
 fi
 
 if [[ "$os" == "OSX" ]]; then
-    . init_osx.sh
+    osx_install
 
     exit 0
 fi
@@ -47,10 +70,8 @@ if [[ "$os" == "LINUX" ]]; then
     . /etc/os-release
 
     case "$ID" in
-        manjaro*) . init_linux_manjaro.sh ;;
-        *)
-            echo "Unknown distro. Exit..."
-            ;;
+        manjaro*) linux_manjaro_install ;;
+        *) echo "Unknown distro. Exit..." ;;
     esac
 
     exit 0
