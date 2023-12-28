8561983b5f57643c61982f06885b532a8c80e844
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2023-12-28 03:18:23 +0300

    [feat]: added ruby installation

--- init_linux_manjaro.sh
+++ init_linux_manjaro.sh
@@ -11,10 +11,14 @@ pamac install autoconf
 echo "Adding RVM keys"
 gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
-echo "Install RVM with ruby"
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
+echo "Installing RVM"
+\curl -sSL https://get.rvm.io | bash
 
 source ~/.rvm/scripts/rvm
+
+echo "Installing ruby"
+rvm get head
+rvm install 3.2 --default
 
 cd ~
 
