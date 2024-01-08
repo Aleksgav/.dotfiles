dd1349e069e8fb5a0574d29fc90a141f6d488530
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2024-01-08 17:14:33 +0300

    [feat]: update conky config

--- conky/arrakis/scripts/c.sh
+++ conky/arrakis/scripts/c.sh
@@ -3,10 +3,6 @@
 # Variable
 temp=$(cat /sys/class/thermal/thermal_zone0/temp)
 cputemp=$(expr $temp / 1000)
-base=32
-
-# Main
-Fahrenheit=$(expr $cputemp \* 9 / 5 + $base)
-echo $Fahrenheit
+echo $cputemp
 
 exit
