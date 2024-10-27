1263519c737763df0732cbad44dd46221dc1d539
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2024-10-27 20:24:15 +0300

    [fix]: sonic pi installation

--- init/packages/linux/manjaro/sonic_pi.rb
+++ init/packages/linux/manjaro/sonic_pi.rb
@@ -4,7 +4,7 @@ module Linux
   module Manjaro
     SonicPi = Packager::Package::Builder.build do
       title 'SonicPI'
-      command 'pamac install sonic-pi --no-confirm'
+      command 'pamac build sonic-pi --no-confirm'
       os TARGET_OS
       distro TARGET_DISTRO
       sudo_require true
