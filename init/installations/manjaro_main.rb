f9591b897b8a0d6896f0ed51ee527ed8f490b66b
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2024-10-27 17:41:59 +0300

    [feat]: disable zeplin

--- init/installations/manjaro_main.rb
+++ init/installations/manjaro_main.rb
@@ -268,7 +268,7 @@ ManjaroMain = Packager::Installation.new do
     package MnjPkg::Discord
     package MnjPkg::Ghidra
     package MnjPkg::Flameshot
-    package MnjPkg::Zeplin
+    # package MnjPkg::Zeplin
     package MnjPkg::Zoom
     package MnjPkg::SonicPi
     package MnjPkg::Redisinsight
