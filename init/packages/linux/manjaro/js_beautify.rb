a81fe100736c7aa0e480b2690413c858da7de94d
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2024-10-27 06:46:41 +0300

    [feat]: js beautify

--- init/packages/linux/manjaro/js_beautify.rb
+++ init/packages/linux/manjaro/js_beautify.rb
@@ -4,7 +4,7 @@ module Linux
   module Manjaro
     JsBeautify = Packager::Package::Builder.build do
       title 'Js beautify'
-      command 'npm -g install js-beautify --no-confirm'
+      command '. $HOME/.config/nvm/nvm.sh && npm -g install js-beautify --no-confirm'
       os TARGET_OS
       distro TARGET_DISTRO
       sudo_require false
