2b532d73876004c8a3d3b2889a28893181b6b5b6
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2025-01-30 04:43:36 +0300

    [feat]: update config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -183,6 +183,10 @@
 ;; PLUGINS
 ;;
 (setq projectile-project-search-path '("~/projects/"))
+;; (after! projectile
+;;   (setq projectile-project-root-files-bottom-up
+;;         (remove ".git" projectile-project-root-files-bottom-up)))
+
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
