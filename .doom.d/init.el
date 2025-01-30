7ce5bad04465b2387acf4b460835af2267b4abff
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2025-01-30 04:43:06 +0300

    [feat]: added php

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -165,7 +165,7 @@
             +journal
             +roam2
             +present)      ; organize your plain life in plain text
-       ;;php               ; perl's insecure younger brother
+       php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        ;;python            ; beautiful is better than ugly
