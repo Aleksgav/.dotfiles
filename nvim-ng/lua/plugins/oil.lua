77579b249fc3ba92c41138bc6d64c4df681ee59b
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2025-09-24 01:45:15 +0300

    [feat]: oil config

--- nvim-ng/lua/plugins/oil.lua
+++ nvim-ng/lua/plugins/oil.lua
@@ -2,10 +2,39 @@ return {
   "stevearc/oil.nvim",
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    columns = {
+      "icon",
+      "permissions",
+      "size",
+      "mtime",
+    },
+    view_options = {
+      show_hidden = true,
+      is_hidden_file = function(name, bufnr)
+        local m = name:match("^%.")
+        return m ~= nil
+      end,
+      is_always_hidden = function(name, bufnr)
+        return false
+      end,
+      case_insensitive = false,
+      sort = {
+        -- sort order can be "asc" or "desc"
+        -- see :help oil-columns to see which columns are sortable
+        { "type", "asc" },
+        { "name", "asc" },
+      },
+      -- Customize the highlight group for the file name
+      highlight_filename = function(entry, is_hidden, is_link_target, is_link_orphan)
+        return nil
+      end,
+    },
+  },
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
+  setup = {},
 }
