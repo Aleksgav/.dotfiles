0d56e947c6891766c19d461da4244b5080a75ff5
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2025-10-08 21:42:42 +0300

    [feat]: update emacs config: roam2 -> roam

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -164,7 +164,7 @@
        ;;ocaml             ; an objective camel
        (org +pretty
             +journal
-            +roam2
+            +roam
             +present)      ; organize your plain life in plain text
        php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
