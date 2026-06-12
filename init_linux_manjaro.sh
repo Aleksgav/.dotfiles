931919a3c10478205741c3347a460194de63bec4
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2026-06-12 11:34:40 -0700

    [feat]: install ruby

--- init_linux_manjaro.sh
+++ init_linux_manjaro.sh
@@ -10,6 +10,11 @@ pamac install autoconf --no-confirm
 
 echo "Install mise"
 curl https://mise.run/bash | sh
+
+source ~/.bashrc
+
+mise use -g ruby@3.2
+
 # pamac install mise --no-confirm
 # echo "Adding RVM keys"
 # gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
