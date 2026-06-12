b9b431741ba06eb969c046b53c107abd7e717baa
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2026-06-12 12:24:08 -0700

    [feat]: remove old commented code

--- init_linux_manjaro.sh
+++ init_linux_manjaro.sh
@@ -19,19 +19,6 @@ mise use -g ruby@3.4.9
 
 source ~/.bashrc
 
-# pamac install mise --no-confirm
-# echo "Adding RVM keys"
-# gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-
-# echo "Installing RVM"
-# \curl -sSL https://get.rvm.io | bash
-
-# source ~/.rvm/scripts/rvm
-
-# echo "Installing ruby"
-# rvm get head
-# rvm install 3.2 --default
-
 cd ~/.dotfiles/init
 
 echo "Installing gems..."
