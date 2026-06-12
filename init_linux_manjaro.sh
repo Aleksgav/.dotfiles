890e58c447e839edd9025346ba234d80b1928956
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2026-06-12 11:20:40 -0700

    [feat]: added no-confirm

--- init_linux_manjaro.sh
+++ init_linux_manjaro.sh
@@ -3,10 +3,10 @@
 echo "Installing Manjaro additons"
 
 echo "Installing make"
-pamac install make
+pamac install make --no-confirm
 
 echo "Installing autoconf"
-pamac install autoconf
+pamac install autoconf --no-confirm
 
 echo "Adding RVM keys"
 gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
