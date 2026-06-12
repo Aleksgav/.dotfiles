fd39237e61bb9146d4c26acf1416116849b49ac2
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2026-06-12 13:12:49 -0700

    [feat]: change getting sudo pass

--- init/lib/cli/commands/install.rb
+++ init/lib/cli/commands/install.rb
@@ -6,28 +6,40 @@ module CLI
       desc 'Installation packages && configs'
       option :dry_run, type: :boolean, default: false, desc: 'Dry run mode', aliases: ['d']
 
-      attr_reader :installation
-      attr_reader :installator
+      attr_reader :installation, :installer
 
       class << self
-        def [](installation, installator)
-          new(installation, installator)
+        def [](installation, installer)
+          new(installation, installer)
         end
       end
 
-      def initialize(installation, installator)
+      def initialize(installation, installer)
         @installation = installation
-        @installator = installator
+        @installer = installer
       end
 
       def call(**options)
         context = Context.new
+        context.pass = read_sudo_password
 
-        context.pass = TTY::Prompt.new.mask('Input sudo password:', required: true) do |val|
-          val.modify :chomp
+        installer.run(installation, context, **options.slice(:dry_run))
+      end
+
+      private
+
+      def read_sudo_password
+        tty = File.open('/dev/tty', 'r+')
+        begin
+          TTY::Prompt.new(input: tty, output: tty)
+                     .mask('Input sudo password:', required: true) { |q| q.modify :chomp }
+        ensure
+          tty.close
         end
-
-        installator.run(installation, context, **options.slice(:dry_run))
+      rescue Errno::ENOENT, Errno::ENXIO, Errno::EIO
+        abort('No controlling terminal (/dev/tty) is available, so the sudo password ' \
+              "cannot be read.\nRun the installer in an interactive terminal on the VM " \
+              '(not through a non-interactive ssh / `vagrant ssh -c` / provisioning command).')
       end
     end
   end
