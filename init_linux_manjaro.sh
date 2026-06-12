ab482c97f953662dd13223a0db1c11c214e4c3f3
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2026-06-12 11:26:24 -0700

    [feat]: install mise

--- init_linux_manjaro.sh
+++ init_linux_manjaro.sh
@@ -8,17 +8,20 @@ pamac install make --no-confirm
 echo "Installing autoconf"
 pamac install autoconf --no-confirm
 
-echo "Adding RVM keys"
-gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+echi "Install mise"
+curl https://mise.run/bash | sh
+# pamac install mise --no-confirm
+# echo "Adding RVM keys"
+# gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
-echo "Installing RVM"
-\curl -sSL https://get.rvm.io | bash
+# echo "Installing RVM"
+# \curl -sSL https://get.rvm.io | bash
 
-source ~/.rvm/scripts/rvm
+# source ~/.rvm/scripts/rvm
 
-echo "Installing ruby"
-rvm get head
-rvm install 3.2 --default
+# echo "Installing ruby"
+# rvm get head
+# rvm install 3.2 --default
 
 cd ~/.dotfiles/init
 
