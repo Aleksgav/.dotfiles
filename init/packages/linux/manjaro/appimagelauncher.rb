8b40f8ea93ced14ae034a13088718ecb870d9b10
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2026-06-12 13:53:49 -0700

    [fix]: appimagelauncher installation

--- init/packages/linux/manjaro/appimagelauncher.rb
+++ init/packages/linux/manjaro/appimagelauncher.rb
@@ -4,7 +4,7 @@ module Linux
   module Manjaro
     Appimagelauncher = Packager::Package::Builder.build do
       title 'Appimagelauncher'
-      command 'pamac install appimagelauncher --no-confirm'
+      command 'pamac build appimagelauncher --no-confirm'
       os TARGET_OS
       distro TARGET_DISTRO
       sudo_require true
