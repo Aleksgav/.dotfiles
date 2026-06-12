7edeb43a48a19c5d843533d691836aecffa0132b
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2026-06-12 14:27:38 -0700

    [feat]: install superfile through pamac

--- init/packages/linux/manjaro/superfile.rb
+++ init/packages/linux/manjaro/superfile.rb
@@ -4,7 +4,7 @@ module Linux
   module Manjaro
     Superfile = Packager::Package::Builder.build do
       title 'Superfile'
-      command 'bash -c "$(curl -sLo- https://superfile.netlify.app/install.sh)"'
+      command 'pamac install superfile'
       os TARGET_OS
       distro TARGET_DISTRO
       sudo_require true
