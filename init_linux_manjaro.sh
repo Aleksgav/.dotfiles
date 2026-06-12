42f277163d3e10bfe8520c1942c9f47f916bb60a
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2026-06-12 12:21:19 -0700

    [feat]: added reload bash

--- init_linux_manjaro.sh
+++ init_linux_manjaro.sh
@@ -17,6 +17,8 @@ mise trust
 mise settings ruby.compile=false
 mise use -g ruby@3.4.9
 
+source ~/.bashrc
+
 # pamac install mise --no-confirm
 # echo "Adding RVM keys"
 # gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
