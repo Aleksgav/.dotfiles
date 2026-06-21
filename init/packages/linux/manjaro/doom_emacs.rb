16095d92af59cd496b17313a4354530a162b313f
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2026-06-21 04:11:12 -0700

    [feat]: doom emacs idempotent installation

--- init/packages/linux/manjaro/doom_emacs.rb
+++ init/packages/linux/manjaro/doom_emacs.rb
@@ -4,8 +4,12 @@ module Linux
   module Manjaro
     DoomEmacs = Packager::Package::Builder.build do
       cmd =<<-CMD
-              git clone --depth 1 https://github.com/doomemacs/doomemacs #{CONFIG_DIR}/emacs
-              #{CONFIG_DIR}/emacs/bin/doom install --env --fonts --force
+              if [ -d #{CONFIG_DIR}/emacs ]; then
+                #{CONFIG_DIR}/emacs/bin/doom sync
+              else
+                git clone --depth 1 https://github.com/doomemacs/doomemacs #{CONFIG_DIR}/emacs
+                #{CONFIG_DIR}/emacs/bin/doom install --env --fonts --force
+              fi
       CMD
 
       title 'DOOM Emacs'
