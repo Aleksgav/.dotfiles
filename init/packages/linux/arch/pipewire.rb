f34d9b16616e508e78072fb67da5f482fa5da0e6
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2026-06-21 05:07:04 -0700

    [feat]: fix pipewire and remove old audio module

--- init/packages/linux/arch/pipewire.rb
+++ init/packages/linux/arch/pipewire.rb
@@ -4,8 +4,10 @@ module Linux
   module Arch
     Pipewire = Packager::Package::Builder.build do
       title 'PipeWire'
+      # Use the real JACK2 server for JACK instead of pipewire-jack; PipeWire
+      # still backs PulseAudio and ALSA. (pipewire-jack conflicts with jack2.)
       command 'pacman -S --needed --noconfirm ' \
-              'pipewire pipewire-pulse pipewire-jack pipewire-alsa'
+              'pipewire pipewire-pulse jack2 pipewire-alsa'
       os TARGET_OS
       distro TARGET_DISTRO
       sudo_require true
