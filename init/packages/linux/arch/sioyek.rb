6a41352c33a44b5c50f9737451f3ba4d7d06ac45
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2026-06-23 21:21:40 -0700

    [feat]: sioyek -> sioyek-dev

--- init/packages/linux/arch/sioyek.rb
+++ init/packages/linux/arch/sioyek.rb
@@ -4,7 +4,7 @@ module Linux
   module Arch
     Sioyek = Packager::Package::Builder.build do
       title 'Sioyek'
-      command 'yay -S --needed --noconfirm sioyek'
+      command 'yay -S --needed --noconfirm sioyek-dev'
       os TARGET_OS
       distro TARGET_DISTRO
       sudo_require false
