7e50ae662fa36606006c5960b52706549646f7dc
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2026-06-25 03:47:53 -0700

    [feat]: added swaync and integrations

---
+++ init/packages/linux/arch/sway_notification_center.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Linux
+  module Arch
+    SwayNotificationCenter = Packager::Package::Builder.build do
+      title 'SwayNotificationCenter'
+      command 'pacman -S --needed --noconfirm swaync'
+      os TARGET_OS
+      distro TARGET_DISTRO
+      sudo_require true
+      post_install { Linker.link 'swaync', "#{CONFIG_DIR}/swaync" }
+    end
+  end
+end
